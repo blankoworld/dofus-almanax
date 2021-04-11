6c5d3490e2d3e659766c171f934dcdbbcbcb7f72
Olivier DOSSMANN <git@dossmann.net>
2021-04-11 13:36:55 +0200

    [Fix] #8 Deletes all mechanize browser options to fetch webpages

--- almanax_next_week.py
+++ almanax_next_week.py
@@ -47,16 +47,8 @@ def download(url):
     """
     # create a browser object
     br = mechanize.Browser()
-    # browser options
-    br.set_handle_equiv(True)
-    br.set_handle_gzip(True)
-    br.set_handle_redirect(True)
-    br.set_handle_referer(True)
-    br.set_handle_robots(False)
     # Follows refresh 0 but not hangs on refresh > 0
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=5)
-    # User-Agent
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     # open a webpage
     response = br.open(url)
     html = response.read()
